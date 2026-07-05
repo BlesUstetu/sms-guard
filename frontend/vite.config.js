ffce33161825734ddd08746313db75edcc3dcd47
BlesUstetu <lionustetu@gmail.com>
2026-07-05 12:30:49 +0700

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+
+  plugins: [react()],
+
+  server: {
+
+    host: true,
+
+    port: 5173,
+
+    open: true
+
+  },
+
+  preview: {
+
+    port: 4173
+
+  },
+
+  build: {
+
+    outDir: "dist",
+
+    sourcemap: false,
+
+    emptyOutDir: true
+
+  }
+
+});
